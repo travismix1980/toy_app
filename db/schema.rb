b83a8ec287d1a56aabd386e9ed4aac95c5d8487d
Travis Mix <travismix1980@gmail.com>
2020-11-07 09:28:24 -0800

    added users and microposts

--- db/schema.rb
+++ db/schema.rb
@@ -12,18 +12,18 @@
 
 ActiveRecord::Schema.define(version: 2020_11_07_162242) do
 
-  create_table 'microposts', force: :cascade do |t|
-    t.text 'content'
-    t.integer 'user_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'email'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
